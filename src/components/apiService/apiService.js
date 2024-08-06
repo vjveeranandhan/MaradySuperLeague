276c1634e0722634e47be13a8568192733b695c3
vjveeranandhan <vjveeranandhan@gmail.com>
2024-08-06 21:34:12 +0200

    change in ip

--- src/components/apiService/apiService.js
+++ src/components/apiService/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
+    baseURL: 'http://3.90.26.72:8000/',
     headers: {
       'Content-Type': 'application/json',
     },
